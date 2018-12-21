46c0436ed26559d8cab12aca40d2b5fced91963e
Oğuzhan SALTAN <oguzhansaltan@yandex.com>
2018-12-22 02:45:25 +0300

    asdasd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,12 +10,53 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            ////********Daire alanını hesaplama
+            //double radius;
+            //double area;
+            //radius = 10.0;
+            //area = radius * radius * 3.1416;
+            //Console.WriteLine("Alan : "+area);
+            ////Tekte de yapabiliriz : 
+            ////double radius=10.0, area=radius*radius*3.1416;
+
+            ////*********İndirimli fiyat hesaplama
+            //decimal price, discount, discounted_price;
+            //price = 19.95M;//decimal tanımlanırken sonuna M eklenmesi lazım
+            //discount = 0.15M;
+            //discounted_price = price - (price * discount);
+            //Console.WriteLine("Discounted price $"+ discounted_price);
+
+
+
+            //string ad, soyad, email, sonuc, ayrac;
+            //ayrac = "*************";
+            //ad = "Oğuzhan";
+            //soyad = "Saltan";
+            //email = "oguzhansaltan@yandex.com";
+            //sonuc = ayrac + "\n" +
+            //    "Ad : " + ad + "\n" +
+            //    "Soyad : " + soyad + "\n" +
+            //    "Email : " + email + "\n" +
+            //    ayrac;
+            //Console.WriteLine(sonuc);
+
+
+
+            //string ad, soyad,sonuc,ayrac="***********";
+            //Console.WriteLine("Adınız : ");
+            //ad = Console.ReadLine();
+            //Console.WriteLine("Soyadınız : ");
+            //soyad = Console.ReadLine();
+            //sonuc = ayrac + "\n" +
+            //    "Ad : " + ad + "\n" +
+            //    "Soyad : " + soyad + "\n" +
+            //    ayrac;
+            //Console.WriteLine(sonuc);
+
+
+
+            Console.ReadKey();
         }
     }
 }
